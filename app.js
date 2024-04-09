9b8a7fc431d1f1f23f8e0fd147be70cef5cc0429
Šárka Flídrová <sarka.flidrova@email.cz>
2024-04-09 16:51:53 +0200

    Add const for identifiers and emails.

--- app.js
+++ app.js
@@ -93,8 +93,28 @@ const processIdentifiers = (identifiers) => {
 }
 
 // Tyto dva řádky uprav podle zadání
-const identifikatory = []
-const emaily = []
+const identifikatory = nezaplaceneObjednavky.map((identifikator) => {
+	return identifikator.id;
+});
+
+console.log(identifikatory);
+
+const emaily = nezaplaceneObjednavky.map((item) => {
+	return item.uzivatel.email;
+});
+console.log(emaily);
 
 processIdentifiers(identifikatory)
 processEmails(emaily)
+
+
+
+// Upravte soubor app.js tak, aby nachystal dvě nová pole identifikatory a emaily.
+
+// Pole const identifikatory = []a const emaily = [] jsou již předpřipravené. Upravte části za rovnítky. Využijte metodu map na poli nezaplaceneObjednavky.
+
+// Zařiďte, aby v prvním poli identifikatory byly pouze čísla, jednotlivá id z původního pole nezaplaceneObjednavky. Představte si, že čísla bude zpracovávat skladový systém, který je potřebuje přesně v této podobě.
+
+// V druhém poli by měly být pouze řetězce, jména s e-maily zákazníků ve formátu Jméno <jmeno@server.cz>, aby se daly snadno zpracovat e-mailovým serverem.
+
+// Poslední dva předchystané řádky nijak neupravujte. Představte si, že místo nich bude jednou kód, který předá vámi vytvořená pole dalším systémům.
